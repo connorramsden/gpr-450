3f13c7e66d5b54aa10e5dcca1a29f31a2b9c839c
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-09-10 14:22:57 -0400

    Started working on Clip controller update
    Writing out notes and incremented keyframe & clip time

--- ULab/Source/ULab/KeyframeAnimationController.cpp
+++ ULab/Source/ULab/KeyframeAnimationController.cpp
@@ -37,6 +37,30 @@ void AKeyframeAnimationController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	/* Step 01: Pre-resolution
+	* Apply time step
+	* One-time, single line of code, applied twice
+	* Increment keyframe time AND clip time by DeltaTime
+	* Utilizes numerical integration for time itself
+	*/
+
+	keyframeTime += DeltaTime;
+	clipTime += DeltaTime;
+
+	/* Step 02: Resolve time
+	* While unresolved, continue to use playback behaviour to determine the NEW keyframe time & clip time
+	* Update behaviour should be looping such that when the clip reaches either end, 
+		it starts playing again from the opposite end in the same direction
+	* There are 7 cases to check for resolution. At least one will terminate the algorithm
+	* Case 01: Reverse Terminus (Playhead passes clip start)
+	* Case 02: Reverse Skip (Playhead enters previous keyframe
+	* Case 03: Reverse (playhead rewinds towards previous [t += -dt])
+	* Case 04: Paused (playhead stays put [t += 0])
+	* Case 05: Forward (playhead advances towards next keyframe [5 += +dt])
+	* Case 06: Forward Skip (playhead enters next keyframe)
+	* Case 07: Forward Terminus (playhead passes clip end)
+	*/
+
 
 }
 
