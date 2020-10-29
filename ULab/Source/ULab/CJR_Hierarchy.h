35ae7fa3fe54271e710865a65640163e8e94979f
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-10-29 10:40:22 -0400

    re-structuring all data structures. Thought about this a lot lately.

--- ULab/Source/ULab/CJR_Hierarchy.h
+++ ULab/Source/ULab/CJR_Hierarchy.h
@@ -3,12 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/ActorComponent.h"
 #include "GameFramework/Actor.h"
 #include "CJR_Hierarchy.generated.h"
 
 /**
- * Core of all hierarchical data
+ * Core of all Hierarchical data
  * Contains no spatial or temporal information
  */
 UCLASS(BlueprintType)
@@ -17,64 +16,52 @@ class ULAB_API AHNode final : public AActor
 	GENERATED_BODY()
 
 public:
-	// The node's Name
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	/// PROPERTIES ///
+
+	// The node's name / identifier
+	UPROPERTY()
 	FString Name;
 
-	// The Mesh that a Node is visualized by
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent * Mesh;
-
-	// Index within the Tree
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	// Index within the Hierarchical tree
+	UPROPERTY()
 	int Index;
-	// Parent's index within the tree
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+
+	// Index of parent within the tree (-1 if root)
+	UPROPERTY()
 	int ParentIndex;
 
-public:
-	AHNode();
+	// The node's visual representation in Space
+	UPROPERTY()
+	UStaticMeshComponent* Mesh;
 
-public:
-	FORCEINLINE FString GetName() const { return Name; }
-	FORCEINLINE int GetIndex() const { return Index; }
-	FORCEINLINE int GetPIndex() const { return ParentIndex; }
-	FORCEINLINE void SetName(const FString NewName) { Name = NewName; }
-	FORCEINLINE void SetIndex(const int NewIndex) { Index = NewIndex; }
-	FORCEINLINE void SetPIndex(const int NewPIndex) { ParentIndex = NewPIndex; }
+	/// METHODS ///
+	AHNode();
 };
 
 /**
  * Effectively a Node pool
- * Nodes are sorted Tree-style, via depth
+ * Nodes are sorted Tree-style, via Depth
  */
 
-UCLASS(BlueprintType)
-class ULAB_API UHierarchy : public UActorComponent
+USTRUCT()
+struct ULAB_API FHierarchy
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Data")
-	TArray<AHNode *> Nodes;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Data")
+	/// PROPERTIES ///
+	UPROPERTY()
+	TArray<AHNode> Nodes;
+	UPROPERTY()
 	int NumNodes;
+	UPROPERTY()
+	bool bIsInitialized;
 
-    bool bIsInitialized;
-public:
-	UHierarchy();
-	~UHierarchy();
+	/// METHODS ///
+	FHierarchy();
+	~FHierarchy();
 
-public:
-	FORCEINLINE TArray<AHNode *> GetNodes() const { return Nodes; }			// get the entire array of nodes
-	FORCEINLINE AHNode * GetNode(const int Index) { return Nodes[Index]; }	// get a specific node by index
-	FORCEINLINE int GetNumNodes() const { return NumNodes; }					// return the number of nodes in the pool
-
-public:
-	// Initialize Nodes with a number of nodes
 	void Init(int NodesToCreate);
-	// Initialize Nodes with a number of nodes and pre-set names
 	void Init(int NodesToCreate, TArray<FString> Names);
-	// Change the values of a Node at the given index
 	void SetNode(int Index, int NewPIndex, FString NewName);
 };
