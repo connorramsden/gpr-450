fd5a937c5661f5a0ac44030e87b78d6d92ddbe31
Connor Ramsden <connor.ramsden@mymail.champlain.edu>
2020-12-01 21:43:26 -0500

    starting to set up VR tools for final project

---
+++ CJR_Animation/Assets/Scripts/HierarchyObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct NodeObj
+{
+	public string name;
+	public string parentName;
+}
+
+[CreateAssetMenu(fileName = "Hierarchy_", menuName = "Tools/Hierarchy", order = 0)]
+public class HierarchyObj : ScriptableObject
+{
+	public List<NodeObj> nodes;
+}
